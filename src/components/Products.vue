e04704d9cb292e85c1cbdf8de58617fe0e9c94e2
Mateus <mateus.bastos@anapps.org.br>
2019-06-04 11:13:44 -0300

    alteracoes nos produtos

---
+++ src/components/Products.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container products">
+    <div class="row">
+      <div class="col-md-3 col-lg-3">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="../assets/logo.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p
+              class="card-text"
+            >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-lg-3">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="../assets/logo.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p
+              class="card-text"
+            >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-lg-3">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="../assets/logo.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p
+              class="card-text"
+            >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.card-img-top {
+  width: 65% !important;
+  margin: auto;
+}
+.products {
+  margin-top: 2rem;
+}
+.row div {
+  margin: auto;
+}
+</style>
+
+<script>
+import Products from "../services/products";
+export default {
+  name: "products",
+
+  data() {
+    return {
+      produto: {
+        name: "",
+        price: "",
+        description: ""
+      },
+      produtos: []
+    };
+  },
+
+
+  methods: {
+    listar() {
+      Products.listar().then(response => {console.log(response)});
+    }
+  },
+
+  
+  mounted() {
+      this.listar();
+  },
+
+};
+</script>
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import Home from './components/Home.vue';
 import Register from './components/Register.vue';
 import Login from './components/Login.vue';
 import Product from './components/Product.vue';
+import Products from './components/Products.vue';
 
 const routes = [
     { path: '/', component: Home, name: 'inicio' },
-    { path: '/register', component: Register, name: 'registrar' },
+    { path: '/registrar', component: Register, name: 'registrar' },
     { path: '/login', component: Login, name: 'login' },
-    { path: '/product/:id', component: Product, name: 'produto'},
+    { path: '/produto/:id', component: Product, name: 'produto'},
+    { path: '/produtos', component: Products, name: 'produtos'},
 ];
 
 export default routes;
